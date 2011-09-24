f76d63dfbf1e576862a10dbb1bca0ff02051e2b9
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-09-24 18:23:42 -0400

    First Haskell program working.

--- Euler-problems/8.hs
+++
@@ -1,9 +0,0 @@
-findGreatestProduct string n_consecutive_digits = maximum products
-
-digits = map (to_i) string
-digitRuns = eachCons digits nConsecutiveDigits
-products = mapWith (*) digitRuns
-max = maximum products
-indices = findIndices (==max) products
-maxDigits = map (!!) digitRuns indices
-to_s = intersperse '\n' [max, indices, maxDigits]
