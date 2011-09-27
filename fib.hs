03ec5872b7a8e0d284e45b6c9a76f14d09a3e01e
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-09-27 15:18:57 -0400

    Segregate out other projects.

--- fib.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
-
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- input.hs
+++
@@ -1,7 +0,0 @@
-main3 = do
-    putStrLn "Hello, what's your name?"
-    name <- getLine
-    putStrLn ("Hey " ++ name ++ ", you rock!")
-
-main = do
-  rs <- sequence [
